14c1351c0e479ee1a2dab40c297a250cae56543d
jeffigy <ejeffydev@gmail.com>
2024-04-23 22:55:35 +0800

    Part 4 d

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const notesRouter = require("./controllers/notes");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
+const userRouter = require("./controllers/user");
+const loginRouter = require("./controllers/login");
 
 mongoose.set("strictQuery", false);
 
@@ -27,6 +29,8 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
